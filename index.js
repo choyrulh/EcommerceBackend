42971fd99fb1af759b1c9a147a58af80c7926c8d
choyrulh <choirul.humam@students.amikom.ac.id>
2024-01-23 13:13:26 +0700

    update connect mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
-const port = 3000;
 
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log(conn.connections);
+    console.log("DB connection successful!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const port = process.env.PORT;
 app.listen(port, () =>
   console.log(`this express app listening on port ${port}!`)
 );
